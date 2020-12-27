4f56eb6e6d5428815fc71ba8803c1cf7ff543dc6
Sam Terfa <samterfa@gmail.com>
2020-12-27 16:14:36 -0600

    Added slackapp

---
+++ slackapp/GCP.R
@@ -0,0 +1,40 @@
+
+# Preload secrets into environmental variables during build. For local testing, define google_cloud_project_number in .Renviron file.
+preloadSecrets <- function(secrets, project_number = Sys.getenv('google_cloud_project_number')){
+  
+  require(dplyr)
+  
+  if(!dir.exists('.secrets')) dir.create('.secrets')
+  
+  # Either grabs local credentials file for local testing or default service account credentials from cloud build.
+  token <- gargle::token_fetch(scopes = 'https://www.googleapis.com/auth/cloud-platform', path = '.secrets/secret_manager.json')
+  
+  for(secret in secrets){
+    
+    endpt <- glue::glue('v1beta1/projects/{project_number}/secrets/{secret}/versions/latest:access')
+    
+    req <- gargle::request_build(method = 'GET', path = endpt, base_url = 'https://secretmanager.googleapis.com/', token = token)
+    
+    res <- gargle::request_make(req)
+    
+    secret_val <- httr::content(res)$payload$data %>% base64enc::base64decode() %>% rawToChar() %>% jsonlite::fromJSON()
+    
+    # By convention in Google Secret Manager, if object has JSON structure...
+    if(grepl('_json', secret)){
+      
+      # Grab each key and preload as environment variable.
+      for(name in names(secret_val)){
+        eval(parse(text = glue::glue('Sys.setenv({name} = "{secret_val[name]}")')))
+      }
+      
+      # ... else value was plain text.
+    }else{
+      
+      # Grab value and preload as environment variable.
+      eval(parse(text = glue::glue('Sys.setenv({name} = "{secret_val}")')))
+      
+    }
+  }
+}
+
+
---
+++ slackapp/Plumber.R
@@ -0,0 +1,51 @@
+
+source('Slack.R')
+source('GCP.R')
+
+require(dplyr)
+
+# Grab project number from Google Compute Engine metadata.  See https://cloud.google.com/compute/docs/storing-retrieving-metadata for details.
+if(gargle:::detect_gce()){
+  # For deployed app.
+  print('Loading project information from GCE.')
+  project_number <- gargle:::gce_metadata_request('project/numeric-project-id') %>% httr::content() %>% rawToChar()
+  project_id <- gargle:::gce_metadata_request('project/project-id') %>% httr::content() %>% rawToChar()
+}else{
+  #  For local testing, define google_cloud_project_number in .Renviron file.
+  print('Loading project information from environment.')
+  project_number <- Sys.getenv('google_cloud_project_number')
+  project_id <- Sys.getenv('google_cloud_project_id')
+}
+
+secretsToLoad <- c('slack_json')
+preloadSecrets(secrets = secretsToLoad, project_number = project_number)
+
+# Swagger docs at ...s/__swagger__/ (needs trailing slash!)
+if(Sys.getenv('PORT') == '') Sys.setenv(PORT = 8000)
+
+#' @apiTitle R Slack Integration
+#' @apiDescription These endpoints allow the user to create custom functions in Slack which call R functions.
+
+#* Initiates our Slack App
+#* @param req The request
+#* @param res The response
+#* @param text The text typed into Slack (If any)
+#* @post /slackapp
+#* @serializer text
+function(req, res, text, ...){
+  
+  require(dplyr)
+  
+  print('Slack Request Incoming')
+  
+  res <- checkSlackAuth(req, res)
+  
+  views_open(token = Sys.getenv('slack_auth_token'), 
+             trigger_id = req$body$trigger_id, 
+             view = view_object(type = 'modal', 
+                                title = text_object(type = 'plain_text', text = 'Testing'), 
+                                blocks = list(button_element(text = text_object(type = 'plain_text', 
+                                                                                text = 'This is a button')))))
+  
+  return('')
+}
