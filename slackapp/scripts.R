8d933c3b094cd4f99acee8565005fd58a4380f1d
Sam Terfa <sam.terfa@reemahealth.com>
2021-10-11 23:18:15 -0500

    testing

--- slackapp/scripts.R
+++ slackapp/scripts.R
@@ -78,28 +78,7 @@ push_next_view <- function(req){
   # Define payload for the views.open method.
   payload <- list()
   payload$view <- read_json('next_modal.json')
- #### payload$trigger_id <- trigger_id
   payload$response_action <- 'push'
   
   payload
-  
-  # # Pull environmental variable storing our app's auth token.
-  # token <- Sys.getenv('slack_auth_token')
-  # 
-  # # Make the post request sending the payload to Slack.
-  # response <- httr::POST('https://slack.com/api/views.push', 
-  #                        body = payload %>% jsonlite::toJSON(auto_unbox = T), 
-  #                        encode = 'json', 
-  #                        httr::content_type_json(), 
-  #                        httr::add_headers(Authorization = glue::glue('Bearer {token}')))
-  # 
-  # # Check the response content for errors.
-  # result <- httr::content(response)
-  # 
-  # # if(!result$ok){
-  # #   stop(result$error)
-  # # }
-  # 
-  # # Return the response content
-  # result
 }
